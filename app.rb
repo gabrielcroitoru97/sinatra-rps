e02040a3d056c4613ef6ddb6b95d31924951d2bc
gabrielcroitoru97 <gabrielcroitoru@hotmail.com>
2024-07-08 03:05:06 +0000

    Rock Paper Scissors Pass

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
+
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:main)
+end
+
+get("/rock") do
+  @poss = ["rock", "paper", "scissors"]
+  @hand = @poss.sample()
+  erb(:rock)
+end
+
+get("/paper") do
+  @poss = ["rock", "paper", "scissors"]
+  @hand = @poss.sample()
+  erb(:paper)
+end
+
+get("/scissors") do
+  @poss = ["rock", "paper", "scissors"]
+  @hand = @poss.sample()
+  erb(:scissors)
 end
